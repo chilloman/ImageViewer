337ca0356f3c0d5923da614a626958a621f60a7c
Vebjørn Hallås <vebjorn.hallas@gmail.com>
2013-08-16 17:54:10 +0200

    Initial upload

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageViewer;
+
+namespace ImageViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            try
+            {
+                var screen = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
+                var width = screen.Width;
+                var height = screen.Height;
+
+                InitializeComponent();
+                pictureBox1.Image = System.Drawing.Image.FromFile(MainForm.imagePath);
+                this.ClientSize = new Size(pictureBox1.Image.Width, pictureBox1.Image.Height);
+                this.MaximumSize = new Size(width, height);
+                pictureBox1.Size = new Size(pictureBox1.Image.Width, pictureBox1.Image.Height);
+                pictureBox1.MaximumSize = new Size(width, height);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("ERROR: 404 FILE NOT FOUND");
+            }
+            catch (System.ArgumentException)
+            {
+                MessageBox.Show("ERROR: NO INPUT IN TEXTFIELD");
+            }
+            catch (System.OutOfMemoryException)
+            {
+                MessageBox.Show("ERROR: UNKNOWN FILE TYPE. PLEASE CHOOSE A IMAGE FILE");
+            }
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class MainForm : Form
+    {
+        public static string imagePath;
+        private Form1 form1;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create new OpenFileDialog instance
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //Set the filter
+            openFileDialog1.Filter = "All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+
+            //Set the initial dir
+            openFileDialog1.InitialDirectory =  "C:";
+
+            //Set the title
+            openFileDialog1.Title = "Open a image file";
+
+            //Make multiselect false
+            openFileDialog1.Multiselect = false;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                imagePath = openFileDialog1.FileName;
+                form1 = new Form1();
+                form1.Show();
+            }
+        }    
+    }
+}
